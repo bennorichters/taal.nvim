f17c5827b3384bc200e09bd616d7feb642b47bf8
Benno Richters <b.richters@gmail.com>
2025-10-19 11:33:29 +0200

    longer response

--- lua/kitt/mock_post.lua
+++ lua/kitt/mock_post.lua
@@ -23,10 +23,12 @@ local response_body = [===[
 }
 ]===]
 
+local response = "The moon is brighter than it was yesterday."
+
 return function(_, opts)
   if opts.stream then
-    for i = 1, 5 do
-      opts.stream(nil, string.format(stream_data, i))
+    for i = 1, #response do
+      opts.stream(nil, string.format(stream_data, string.sub(response, i, i)))
     end
     opts.stream(nil, stream_done)
   else
