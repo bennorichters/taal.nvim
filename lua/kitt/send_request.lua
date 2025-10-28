b37dae5bde946ffdb1ea366e67c3743489b077dc
Benno Richters <b.richters@gmail.com>
2025-10-28 15:15:45 +0100

    don't log API key 🙊

--- lua/kitt/send_request.lua
+++ lua/kitt/send_request.lua
@@ -2,6 +2,8 @@ local log = require("kitt.log")
 
 return function(post, endpoint, api_key)
   return function(body_content, extra_opts)
+    log.fmt_trace("posting with endpoint=%s, extra_opts=%s", endpoint, extra_opts)
+
     local opts = {
       body = body_content,
       headers = {
@@ -13,8 +15,6 @@ return function(post, endpoint, api_key)
     if extra_opts then
       opts = vim.tbl_deep_extend("error", opts, extra_opts)
     end
-
-    log.fmt_trace("posting with endpoint=%s, opts=%s", endpoint, opts)
 
     return post(endpoint, opts)
   end
