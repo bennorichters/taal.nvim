d10557fce36fd01a83a0e6ba7171801cb7ef88db
Benno Richters <b.richters@gmail.com>
2025-11-09 17:28:36 +0100

    code formatting

--- lua/taal/diff.lua
+++ lua/taal/diff.lua
@@ -1,25 +1,25 @@
 local function split_into_words(text)
-	local prev_whitespace = true
-	local index = 0
+  local prev_whitespace = true
+  local index = 0
 
-	local words = {}
-	local starts = {}
-	for char in string.gmatch(text .. " ", ".") do
-		if string.match(char, "%s") then
-			if not prev_whitespace then
-				table.insert(words, string.sub(text, starts[#starts] + 1, index))
-			end
+  local words = {}
+  local starts = {}
+  for char in string.gmatch(text .. " ", ".") do
+    if string.match(char, "%s") then
+      if not prev_whitespace then
+        table.insert(words, string.sub(text, starts[#starts] + 1, index))
+      end
 
-			prev_whitespace = true
-		elseif prev_whitespace then
-			prev_whitespace = false
-			table.insert(starts, index)
-		end
+      prev_whitespace = true
+    elseif prev_whitespace then
+      prev_whitespace = false
+      table.insert(starts, index)
+    end
 
-		index = index + 1
-	end
+    index = index + 1
+  end
 
-	return table.concat(words, "\n"), starts
+  return table.concat(words, "\n"), starts
 end
 
 local function diff_boundaries(word_columns, index_begin, word_count, length)
@@ -27,39 +27,39 @@ local function diff_boundaries(word_columns, index_begin, word_count, length)
     return 0, 0
   end
 
-	local column_start = word_columns[index_begin]
-	local index_end = index_begin + word_count
-	local column_end = index_end <= #word_columns and (word_columns[index_end] - 1) or length
+  local column_start = word_columns[index_begin]
+  local index_end = index_begin + word_count
+  local column_end = index_end <= #word_columns and (word_columns[index_end] - 1) or length
 
-	return column_start, column_end
+  return column_start, column_end
 end
 
 local M = {}
 
 M.diff = function(a, b)
-	local a_words, a_starts = split_into_words(a)
-	local b_words, b_starts = split_into_words(b)
+  local a_words, a_starts = split_into_words(a)
+  local b_words, b_starts = split_into_words(b)
 
-	local indices = vim.diff(a_words, b_words, { result_type = "indices" })
+  local indices = vim.diff(a_words, b_words, { result_type = "indices" })
 
-	local result = {}
-	if type(indices) == "table" then
-		for _, start_index in ipairs(indices) do
-			local a_start, a_end = diff_boundaries(a_starts, start_index[1], start_index[2], #a_words)
-			local b_start, b_end = diff_boundaries(b_starts, start_index[3], start_index[4], #b_words)
+  local result = {}
+  if type(indices) == "table" then
+    for _, start_index in ipairs(indices) do
+      local a_start, a_end = diff_boundaries(a_starts, start_index[1], start_index[2], #a_words)
+      local b_start, b_end = diff_boundaries(b_starts, start_index[3], start_index[4], #b_words)
 
-			table.insert(result, {
-				a_start = a_start,
-				a_end = a_end,
-				a_text = string.sub(a, a_start + 1, a_end),
-				b_start = b_start,
-				b_end = b_end,
-				b_text = string.sub(b, b_start + 1, b_end),
-			})
-		end
-	end
+      table.insert(result, {
+        a_start = a_start,
+        a_end = a_end,
+        a_text = string.sub(a, a_start + 1, a_end),
+        b_start = b_start,
+        b_end = b_end,
+        b_text = string.sub(b, b_start + 1, b_end),
+      })
+    end
+  end
 
-	return result
+  return result
 end
 
 return M
